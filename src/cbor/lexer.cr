9d1c255c2c3b61cb068424ae8fa770480c792d7b
Alberto Restifo <alberto@restifo.dev>
2020-04-19 00:18:54 +0200

    Start lexer and tests

--- src/cbor/lexer.cr
+++ src/cbor/lexer.cr
@@ -1,67 +1,97 @@
 require "./token"
 
 class CBOR::Lexer
-  @current_byte : UInt8
-
-  def initialize(@io : IO)
-    @current_byte = 0x0
+  def self.new(string : String)
+    new IO::Memory.new(string)
   end
 
-  def next_token : Token?
-    byte = @io.read_byte
-    return nil if byte.nil?
+  def self.new(slice : Bytes)
+    new IO::Memory.new(slice)
+  end
 
-    @current_byte = byte
+  @token : Token::T
 
-    # See: RFC7049 Appedix B
-    case @current_byte
-    when .<= 0x17
-      Token.new(kind: Token::Kind::UInt, value: @curren_byte)
-    when 0x18
-      read_uint8
-    when 0x19
-      read_uint16
-    when 0x1a
-      read_uint32
-    when 0x1b
-      read_uint64
-    when .<= 0x37
-      Token.new(kind: Token::Kind::NInt, value: Int8(@curren_byte))
+  def initialize(@io : IO)
+    @byte_number = 0
+    @current_byte_number = 0
+    @token = Token::NullT.new(0)
+    @token_finished = true
+  end
+
+  @[AlwaysInline]
+  def current_token : Token::T
+    if @token_finished
+      @token_finished = false
+      @token = next_token
+    else
+      @token
     end
   end
 
-  private def read_uint8
+  @[AlwaysInline]
+  def finish_token!
+    @token_finished = true
+  end
+
+  @[AlwaysInline]
+  def read_token : Token::T
+    if @token_finished
+      @token = next_token
+    else
+      finish_token!
+    end
+    @token
+  end
+
+  private def next_token
+    @current_byte_number = @byte_number
+    current_byte = next_byte
+
+    case current_byte
+    when 0x00..0x17
+      consume_int(current_byte)
+    when 0x18
+      consume_int(read(Uint8))
+    when 0x19
+      consume_int(read(Uint16))
+    when 0x1a
+      consume_int(read(Uint32))
+    when 0x1b
+      consume_int(read(Uint64))
+    when 0x20..0x37
+      consume_int(flip(current_byte.to_i8))
+    when 0x38
+      consume_int(flip(read(Uint8).to_i8))
+    when 0x39
+      consume_int(flip(read(Uint16).to_i16))
+    when 0x3a
+      consume_int(flip(read(Uint32).to_i32))
+    when 0x3b
+      consume_int(flip(read(Uint64).to_i64))
+    end
+  end
+
+  private def next_byte : Uint8
     byte = @io.read_byte
-    return unexpect_eof if byte.nil?
-
-    Token.new(kind: Token::Kind::UInt, value: byte)
+    @byte_number += 1
+    fail unless byte
+    byte
   end
 
-  private def read_uint16
-    value = UInt16.from_io(read_next(2), IO::ByteFormat::BigEndian)
-    Token.new(kind: Token::Kind::UInt, value: value)
+  private def consume_int(value)
+    Token::IntT.new(@current_byte_number, value)
   end
 
-  private def read_uint32
-    value = UInt32.from_io(read_next(4), IO::ByteFormat::BigEndian)
-    Token.new(kind: Token::Kind::UInt, value: value)
+  private def flip(value)
+    -1 - value
   end
 
-  private def read_uint64
-    value = UInt64.from_io(read_next(8), IO::ByteFormat::BigEndian)
-    Token.new(kind: Token::Kind::UInt, value: value)
+  private def read(type : T.class) forall T
+    @byte_number += sizeof(T)
+    @io.read_bytes(T, IO::ByteFormat::NetworkEndian)
   end
 
-  private def read_next(n : Int)
-    slice = Bytes.new(n)
-
-    read = @io.read(slice)
-    return unexpect_eof if read == 0
-
-    slice
-  end
-
-  private def unexpected_eof
-    raise "Unexpected EOF"
+  private def fail
+    raise "Pase error"
   end
 end
--- src/cbor/token.cr
+++ src/cbor/token.cr
@@ -1,14 +1,21 @@
-class CBOR::Token(T)
-  enum Kind
-    UInt8
-    NInt
-    Byte
-    Text
-    Array
-    Map
-    Float
-  end
+class CBOR::Token
+  record NullT, byte_number : Int32
+  record BoolT, byte_number : Int32, value : Bool
+  record ArrayT, byte_number : Int32, size : UInt32
+  record MapT, byte_number : Int32, size : UInt32
+  record IntT, byte_number : Int32, value : Int8 | UInt8 | Int16 | UInt16 | Int32 | UInt32 | Int64 | UInt64
+  record FloatT, byte_number : Int32, value : Float64
+  record StringT, byte_number : Int32, value : String
+  record BytesT, byte_number : Int32, value : Bytes
 
-  def initialize(@value : T, @kind : Kind)
+  alias T = NullT | BoolT | ArrayT | MapT | IntT | FloatT | StringT | BytesT
+
+  def self.to_s(token : T)
+    case token
+    when IntT
+      token.value.to_s
+    else
+      "NOT IMPLEMENTED YET!"
+    end
   end
 end
